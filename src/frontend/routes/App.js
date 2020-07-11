e0d6067e62d25250efb27aabf87e4dc9912faa06
fbarrios <freddybarrios.077@gmail.com>
2020-07-11 14:56:33 -0500

    first commit

--- src/frontend/routes/App.js
+++ src/frontend/routes/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
-import Register from '../containers/Register';
-import NotFound from '../containers/NotFound';
-import Player from '../containers/Player';
-import Layout from '../components/Layout';
+import Home from '../../containers/Home';
+import Login from '../../containers/Login';
+import Register from '../../containers/Register';
+import NotFound from '../../containers/NotFound';
+import Player from '../../containers/Player';
+import Layout from '../../components/Layout';
 
 const App = () => (
   <BrowserRouter>
